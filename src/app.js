cb743bcffbc44a70712b9d403d7ec9d89a745a05
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-07-21 14:07:06 -0300

    adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -12,5 +12,7 @@ app.use(router);
 dotenv.config(); 
 
 
-const PORT = 5000; 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
